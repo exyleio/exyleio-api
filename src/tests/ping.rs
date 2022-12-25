2f5bf8e6c4cf89b946ffc8ea09d66ba57cd2da53
developomp <developomp@gmail.com>
2022-12-25 14:11:02 +0900

    organized testing code

--- src/tests/ping.rs
+++ src/tests/ping.rs
@@ -1,8 +1,9 @@
+use crate::rocket;
 use rocket::local::blocking::Client;
 
 #[test]
 fn ping() {
-    let client = Client::tracked(super::rocket()).unwrap();
+    let client = Client::tracked(rocket()).unwrap();
     let response = client.get("/ping").dispatch();
     assert_eq!(response.into_string().unwrap(), "PONG");
 }
