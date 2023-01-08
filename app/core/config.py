f047017d620f4f1fcb3809bd1e4a2c61b6810196
developomp <developomp@gmail.com>
2023-01-08 21:43:06 +0900

    chore: add comments

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,10 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    # both the API server and the redis server should be running inside docker
+    # for this to work
     REDIS_ADDRESS: str = "redis://redis:6379"
+
     API_V1_STR: str = "/v1"
 
 
