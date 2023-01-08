6989b02b80cb5e97340a0f5ce03b375529fc2490
developomp <developomp@gmail.com>
2023-01-09 00:07:20 +0900

    refactor: move db schema doc to `db.py`

--- app/db.py
+++ app/db.py
@@ -1,3 +1,18 @@
+"""
+Data models
+
+Database Schema:
+
+| Key                                        | Type       | Field/Member         | Value/Score                                    |
+| ------------------------------------------ | ---------- | -------------------- | ---------------------------------------------- |
+| rank:<game-mode>                           | Sorted Set | player id            | ranking criteria                               |
+| rank:<game-mode>:days-number-one           | Sorted Set | player id            | total number of days player was in the #1 rank |
+| rank:<game-mode>:longest-number-one-streak | Sorted Set | player id            | player's longest #1 streak                     |
+| rank:xp                                    | Sorted Set | player id            | Player XP                                      |
+| player:<player-id>:items                   | Set        | item id              | -                                              |
+| player:<id>                                | Hash       | models.player.Player | in-exhaustive player data                      |
+"""
+
 import redis
 from app.core.config import settings
 
